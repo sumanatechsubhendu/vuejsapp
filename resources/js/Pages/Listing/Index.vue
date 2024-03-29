444d6d7c4a2d528e04ad3600810f92a92338f12c
subhendu das <subhendu.das@arkinfotec.com>
2024-03-29 11:59:20 +0530

    layout added in listing

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,15 +1,21 @@
 <template>
-  <Filters :filters="filters" />
-  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-    <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
-  </div>
+    <Head title="Dashboard" />
 
-  <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
-    <Pagination :links="listings.links" />
-  </div>
+    <AuthenticatedLayout>
+        <Filters :filters="filters" />
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+            <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
+        </div>
+
+        <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
+            <Pagination :links="listings.links" />
+        </div>
+    </AuthenticatedLayout>
 </template>
 
 <script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
 import Pagination from '@/Components/UI/Pagination.vue'
 import Filters from '@/Pages/Listing/Index/Components/Filters.vue'
